e4f556969853f75a9ed50373f9f5c6e96e3c6b37
chyzio <chyzio89@gmai.com>
2012-07-05 14:10:01 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?
+
+ini_set( 'display_errors', 'On' );
+error_reporting( E_ALL );
+
+require 'libs/smarty/Smarty.class.php';
+require 'libs/class/db_connect.class.php';
+
+$smarty = new Smarty;
+
+$smarty->compile_check = 0;
+$smarty->debugging = 0;
+//var_dump($db_connect);
+
+?>
