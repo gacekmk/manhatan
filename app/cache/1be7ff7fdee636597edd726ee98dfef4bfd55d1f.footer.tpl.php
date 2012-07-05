4f5b120d4c0ad029355befc311b98cee79a6c028
chyzio <chyzio89@gmai.com>
2012-07-05 13:56:26 +0200

    smarty

---
+++ app/cache/1be7ff7fdee636597edd726ee98dfef4bfd55d1f.footer.tpl.php
@@ -0,0 +1,20 @@
+<?php /*%%SmartyHeaderCode:31664ff17603648909-10881283%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '1be7ff7fdee636597edd726ee98dfef4bfd55d1f' => 
+    array (
+      0 => '.\\templates\\footer.tpl',
+      1 => 1341224571,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '31664ff17603648909-10881283',
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_4ff17e05087c88_35211345',
+  'has_nocache_code' => false,
+  'cache_lifetime' => 120,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_4ff17e05087c88_35211345')) {function content_4ff17e05087c88_35211345($_smarty_tpl) {?></body>
+</html>
+<?php }} ?>
